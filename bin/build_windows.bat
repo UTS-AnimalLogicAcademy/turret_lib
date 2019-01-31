23420094ecefe371d1f0e21b963518eb38ea9765
Benjamin Skinner <Benjamin.Skinner@uts.edu.au>
2019-01-31 15:22:51 +1100

    Added batch file for windows building

---
+++ bin/build_windows.bat
@@ -0,0 +1,15 @@
+@echo off
+
+rd /s /q build
+
+mkdir build
+cd build
+
+SET CPPZMQ_INCLUDE=C:/Users/142738/git/cppzmq
+SET LIBZMQ_INCLUDE_DIR=C:/Users/142738/git/libzmq/include
+SET LIBZMQ_LIB_DIR=C:/Users/142738/git/libzmq/build/lib/Release/libzmq-v140-mt-4_3_2.lib
+
+cmake -DCMAKE_INSTALL_PREFIX="C:\Users\142738\software\turret-lib" -G "Visual Studio 14 2015 Win64" ..
+cmake --build . --config Release --target install -- /M:16
+
+cd ..
