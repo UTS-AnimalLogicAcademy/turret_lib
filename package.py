b80d6d468b2f6fa34ecd49c993d6400175ec8792
daniel flood <daniel.flood-1@uts.edu.au>
2021-02-19 15:43:50 +1100

    more variant juggling in rez package.py

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'turret_lib'
 
-version = '1.2.3'
+version = '1.2.4'
 
 authors = [ 'ben.skinner',
             'daniel.flood'
@@ -17,10 +17,12 @@ private_build_requires = [
 ]
 
 variants = [
-  ['platform-linux', 'arch-x86_64', 'tbb-4', 'boost-1.55'],
+  ['platform-linux', 'arch-x86_64', 'tbb-4', 'boost-1.55'], # older usd versions
+  ['platform-linux', 'arch-x86_64', 'tbb-2017.0', 'boost-1.55', 'devtoolset-6'], # maya 2019
+  ['platform-linux', 'arch-x86_64', 'tbb-2017.8', 'boost-1.61', 'devtoolset-6'],
   ['platform-linux', 'arch-x86_64', 'tbb_katana-2017', 'boost_katana-1.61'],
-  ['platform-linux', 'arch-x86_64', 'tbb-2019.0', 'boost-1.61'],# houdini 18.0
-  ['platform-linux', 'arch-x86_64', 'tbb-2019.9', 'boost-1.72'] # houdini 18.5
+  ['platform-linux', 'arch-x86_64', 'tbb-2019.0', 'boost-1.61', 'devtoolset-6'],# houdini 18.0
+  ['platform-linux', 'arch-x86_64', 'tbb-2019.9', 'boost-1.72', 'devtoolset-6'] # houdini 18.5
 ]
 
 def commands():
