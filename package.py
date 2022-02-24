b7535ef7147059a8600e0a029eabd090b3bf95e0
Naomi Que <149199@ocelot.ala.int.uts.edu.au>
2022-02-24 15:26:49 +1100

    Added a build variant for maya-2022

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'turret_lib'
 
-version = '1.2.6'
+version = '1.2.8'
 
 authors = [ 'ben.skinner',
             'daniel.flood'
@@ -22,7 +22,8 @@ variants = [
   ['platform-linux', 'arch-x86_64', 'tbb-2017.8', 'boost-1.61', 'devtoolset-6'],
   ['platform-linux', 'arch-x86_64', 'tbb_katana-2017', 'boost_katana-1.61'],
   ['platform-linux', 'arch-x86_64', 'tbb-2019.0', 'boost-1.61', 'devtoolset-6'],# houdini 18.0
-  ['platform-linux', 'arch-x86_64', 'tbb-2019.9', 'boost-1.72', 'devtoolset-6'] # houdini 18.5
+  ['platform-linux', 'arch-x86_64', 'tbb-2019.9', 'boost-1.72', 'devtoolset-7'], # houdini 18.5
+  ['platform-linux', 'arch-x86_64', 'tbb-2017.0', 'boost-1.61', 'devtoolset-6'] # maya 2022
 ]
 
 def commands():
