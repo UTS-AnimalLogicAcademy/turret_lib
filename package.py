4904139f68cb776d47406f26e2c4cad5ed9c5530
Jonah Newton <jonah.newton@uts.edu.au>
2022-05-16 11:03:20 +1000

    Added variant for boost 1.70 and tbb-2019.6

--- package.py
+++ package.py
@@ -2,10 +2,11 @@
 
 name = 'turret_lib'
 
-version = '1.2.8'
+version = '1.2.9'
 
 authors = [ 'ben.skinner',
-            'daniel.flood'
+            'daniel.flood',
+	    'jonah.newton'
 ]
 
 requires = ['libzmq-4',
@@ -23,7 +24,8 @@ variants = [
   ['platform-linux', 'arch-x86_64', 'tbb_katana-2017', 'boost_katana-1.61'],
   ['platform-linux', 'arch-x86_64', 'tbb-2019.0', 'boost-1.61', 'devtoolset-6'],# houdini 18.0
   ['platform-linux', 'arch-x86_64', 'tbb-2019.9', 'boost-1.72', 'devtoolset-7'], # houdini 18.5
-  ['platform-linux', 'arch-x86_64', 'tbb-2017.0', 'boost-1.61', 'devtoolset-6'] # maya 2022
+  ['platform-linux', 'arch-x86_64', 'tbb-2017.0', 'boost-1.61', 'devtoolset-6'], # maya 2022
+  ['platform-linux', 'arch-x86_64', 'tbb-2019.6', 'boost-1.70', 'devtoolset-6'] # katana 4.5v2
 ]
 
 def commands():
