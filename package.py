e25623e44a8b1a37bf10266cac0fdf7049f938ba
Jonah Newton <jonah.newton@uts.edu.au>
2023-07-20 13:22:59 +1000

    Fixed Katana variants

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'turret_lib'
 
-version = '1.2.10'
+version = '1.2.11'
 
 authors = [ 'ben.skinner',
             'daniel.flood',
@@ -18,15 +18,18 @@ private_build_requires = [
 ]
 
 variants = [
-  ['platform-linux', 'arch-x86_64', 'tbb-4', 'boost-1.55'], # older usd versions
-  ['platform-linux', 'arch-x86_64', 'tbb-2017.0', 'boost-1.55', 'devtoolset-6'], # maya 2019
-  ['platform-linux', 'arch-x86_64', 'tbb-2017.8', 'boost-1.61', 'devtoolset-6'],
+  ['platform-linux', 'arch-x86_64', 'tbb-4', 'boost-1.55', '!katana'], # older usd versions
+  ['platform-linux', 'arch-x86_64', 'tbb-2017.0', 'boost-1.55', 'devtoolset-6', '!katana'], # maya 2019
+  ['platform-linux', 'arch-x86_64', 'tbb-2020.3', 'boost-1.76', 'devtoolset-7', '!katana'], # maya 2023
+  ['platform-linux', 'arch-x86_64', 'tbb-2017.8', 'boost-1.61', 'devtoolset-6', '!katana'],
   ['platform-linux', 'arch-x86_64', 'tbb_katana-2017', 'boost_katana-1.61'],
-  ['platform-linux', 'arch-x86_64', 'tbb-2019.0', 'boost-1.61', 'devtoolset-6'],# houdini 18.0
-  ['platform-linux', 'arch-x86_64', 'tbb-2019.9', 'boost-1.72', 'devtoolset-7'], # houdini 18.5
-  ['platform-linux', 'arch-x86_64', 'tbb-2019.9', 'boost-1.72', 'devtoolset-6'], # houdini 19
-  ['platform-linux', 'arch-x86_64', 'tbb-2017.0', 'boost-1.61', 'devtoolset-6'], # maya 2022
-  ['platform-linux', 'arch-x86_64', 'tbb-2019.6', 'boost-1.70', 'devtoolset-6'] # katana 4.5v2
+  ['platform-linux', 'arch-x86_64', 'tbb_katana-2020.3', 'boost_katana-1.76'], #Katana 6
+  ['platform-linux', 'arch-x86_64', 'tbb-2019.0', 'boost-1.61', 'devtoolset-6', '!katana'],# houdini 18.0
+  ['platform-linux', 'arch-x86_64', 'tbb-2019.9', 'boost-1.72', 'devtoolset-7', '!katana'], # houdini 18.5
+  ['platform-linux', 'arch-x86_64', 'tbb-2019.9', 'boost-1.72', 'devtoolset-6', '!katana'], # houdini 19
+  ['platform-linux', 'arch-x86_64', 'tbb-2017.0', 'boost-1.61', 'devtoolset-6', '!katana'], # maya 2022
+  ['platform-linux', 'arch-x86_64', 'tbb_katana-2019.6', 'boost_katana-1.70', 'devtoolset-6'], # katana 4.5v2 / katana 5
+  ['platform-linux', 'arch-x86_64', 'tbb_katana-2020.3', 'boost_katana-1.76', 'devtoolset-6'] #katana 6
 ]
 
 def commands():
