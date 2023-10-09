ca6286c9126ca8d3f15af067b2fdebe6d55b8f19
Jonah Newton <jonah.newton@uts.edu.au>
2023-10-10 10:07:48 +1100

    Add variant for Nuke 12.2v1

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'turret_lib'
 
-version = '1.2.11'
+version = '1.2.12'
 
 authors = [ 'ben.skinner',
             'daniel.flood',
@@ -23,13 +23,13 @@ variants = [
   ['platform-linux', 'arch-x86_64', 'tbb-2020.3', 'boost-1.76', 'devtoolset-7', '!katana'], # maya 2023
   ['platform-linux', 'arch-x86_64', 'tbb-2017.8', 'boost-1.61', 'devtoolset-6', '!katana'],
   ['platform-linux', 'arch-x86_64', 'tbb_katana-2017', 'boost_katana-1.61'],
-  ['platform-linux', 'arch-x86_64', 'tbb_katana-2020.3', 'boost_katana-1.76'], #Katana 6
   ['platform-linux', 'arch-x86_64', 'tbb-2019.0', 'boost-1.61', 'devtoolset-6', '!katana'],# houdini 18.0
   ['platform-linux', 'arch-x86_64', 'tbb-2019.9', 'boost-1.72', 'devtoolset-7', '!katana'], # houdini 18.5
   ['platform-linux', 'arch-x86_64', 'tbb-2019.9', 'boost-1.72', 'devtoolset-6', '!katana'], # houdini 19
   ['platform-linux', 'arch-x86_64', 'tbb-2017.0', 'boost-1.61', 'devtoolset-6', '!katana'], # maya 2022
   ['platform-linux', 'arch-x86_64', 'tbb_katana-2019.6', 'boost_katana-1.70', 'devtoolset-6'], # katana 4.5v2 / katana 5
-  ['platform-linux', 'arch-x86_64', 'tbb_katana-2020.3', 'boost_katana-1.76', 'devtoolset-6'] #katana 6
+  ['platform-linux', 'arch-x86_64', 'tbb_katana-2020.3', 'boost_katana-1.76', 'devtoolset-6'], #katana 6
+  ['platform-linux', 'arch-x86_64', 'tbb-2018.4', 'boost-1.66', '!katana'] #nuke 12.2v1
 ]
 
 def commands():
