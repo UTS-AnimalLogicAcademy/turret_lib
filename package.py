f18fcda582c249fe51bc0847e4de162427814db1
Jonah Newton <jonah.newton@uts.edu.au>
2024-02-26 16:45:13 +1100

    Variant 3 builds for both Maya and Nuke

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'turret_lib'
 
-version = '1.3.4'
+version = '1.3.5'
 
 authors = [ 'ben.skinner',
             'daniel.flood',
@@ -21,7 +21,7 @@ private_build_requires = [
 variants = [
   ['platform-linux', 'arch-x86_64', 'tbb-4', 'boost-1.55', '!katana'], # older usd versions
   ['platform-linux', 'arch-x86_64', 'tbb-2017.0', 'boost-1.55', '!katana', '!nuke'], # maya 2019
-  ['platform-linux', 'arch-x86_64', 'tbb-2020.3', 'boost-1.76', '!katana', '!nuke'], # maya 2023
+  ['platform-linux', 'arch-x86_64', 'tbb-2020.3', 'boost-1.76', '!katana'], # maya 2023/Nuke 14.1
   ['platform-linux', 'arch-x86_64', 'tbb-2017.8', 'boost-1.61', '!katana'],
   ['platform-linux', 'arch-x86_64', 'tbb_katana-2017', 'boost_katana-1.61', '!maya'],
   ['platform-linux', 'arch-x86_64', 'tbb-2019.0', 'boost-1.61', '!katana', '!maya', '!nuke'],# houdini 18.0
@@ -30,7 +30,7 @@ variants = [
   ['platform-linux', 'arch-x86_64', 'tbb-2020.3', 'boost-1.72.1', 'python-3.7', '!katana', '!nuke'], # houdini 19.5
   ['platform-linux', 'arch-x86_64', 'tbb-2020.3', 'boost-1.72.1', 'python-3.10', '!katana', '!nuke'], # houdini 20
   ['platform-linux', 'arch-x86_64', 'tbb-2017.0', 'boost-1.61', '!katana', '!nuke'], # maya 2022, usd-20.11
-  ['platform-linux', 'arch-x86_64', 'tbb_katana-2019.6', 'boost_katana-1.70', '!maya', '!nuke'], # katana 4.5v2 / katana 5
+  ['platform-linux', 'arch-x86_64', 'tbb_katana-2019.6', 'boost_katana-1.70', 'katana-4.5.2', '!maya', '!nuke'], # katana 4.5v2
   ['platform-linux', 'arch-x86_64', 'tbb_katana-2020.3', 'boost_katana-1.76', '!maya', '!nuke'], #katana 6
   ['platform-linux', 'arch-x86_64', 'tbb-2018.4', 'boost-1.66', '!katana', '!maya'], #nuke 12.2v1
   ['platform-linux', 'arch-x86_64', 'tbb-2020.3', 'boost-1.80', '!maya', '!nuke', '!katana'] #usd 23.08 standalone
