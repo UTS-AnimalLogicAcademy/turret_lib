10aaa6bcafad59894358e180847652f09fad8d29
Jonah Newton <jonah.newton@uts.edu.au>
2024-04-18 11:52:43 +1000

    Add H20.5 variant

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'turret_lib'
 
-version = '1.5.0'
+version = '1.6.0'
 
 authors = [ 'ben.skinner',
             'daniel.flood',
@@ -15,7 +15,7 @@ requires = ['libzmq-4',
 
 private_build_requires = [
     'cmake-3.12',
-    'devtoolset-7+'
+    'devtoolset'
 ]
 
 variants = [
@@ -30,6 +30,7 @@ variants = [
   ['platform-linux', 'arch-x86_64', 'tbb-2020.3', 'boost-1.72.1', 'python-3.7', '!tbb_katana','!boost_katana', '!nuke'], # houdini 19.5 3.7
   ['platform-linux', 'arch-x86_64', 'tbb-2020.3', 'boost-1.72.1', 'python-3.9', '!tbb_katana','!boost_katana', '!nuke'], # houdini 19.5 3.9
   ['platform-linux', 'arch-x86_64', 'tbb-2020.3', 'boost-1.72.1', 'python-3.10', '!tbb_katana','!boost_katana', '!nuke'], # houdini 20
+  ['platform-linux', 'arch-x86_64', 'tbb-2020.3', 'boost-1.82', 'python-3.10', '!tbb_katana','!boost_katana', '!nuke'], # houdini 20.5 py3.10
   ['platform-linux', 'arch-x86_64', 'tbb-2017.0', 'boost-1.61', '!tbb_katana','!boost_katana', '!nuke'], # maya 2022, usd-20.11
   ['platform-linux', 'arch-x86_64', 'tbb_katana-2019.6', 'boost_katana-1.70', '!tbb', '!boost', 'katana-4.5.2', '!maya', '!nuke', '!blender'], # katana 4.5v2
   ['platform-linux', 'arch-x86_64', 'tbb_katana-2020.3', 'boost_katana-1.76', '!tbb', '!boost', '!maya', '!nuke', '!blender', '!blender'], #katana 6
